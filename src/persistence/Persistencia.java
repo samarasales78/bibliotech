bd9817899346412f296daa3867141c1d517bcae9
samarasales78 <smatiassales@gmail.com>
2026-07-25 21:14:25 -0300

    move data files outside src directory

--- src/persistence/Persistencia.java
+++ src/persistence/Persistencia.java
@@ -2,7 +2,7 @@ package persistence;
 
 import java.io.IOException;
 
-    // Define operações básicas de persistência dos dados do sistema.
+// Define operações básicas de persistência dos dados do sistema.
 public interface Persistencia {
     void carregarDados() throws IOException;
 
